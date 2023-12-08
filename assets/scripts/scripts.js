e1684146d40fc9a57b68b5ffb8bf2687836f8897
Lucas Garcia Buixens <Lucasgb@hotmail.com>
2023-12-08 17:15:54 -0300

    footer y formulario

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -5,64 +5,14 @@ window.addEventListener('scroll', function () {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-/*Swiper initialize
-**************************************/
-if (document.querySelector('.swiper')) {
-  const swiper = new Swiper('.swiper', {
-      direction: "horizontal",
-      loop: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false
-      },
-      effect: "fade",
-      speed: 1000,
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      }
-  });
-}
 
 
 /*Update copy automatically
 **************************************************/
 document.querySelector(".year-copy").innerText = new Date().getFullYear();
 
-/*Light Gallery
-*********************************************/
-document.querySelectorAll('.gallery-container').forEach((element, index) => {
-  lightGallery(document.getElementById(element.getAttribute('id')), {
-    plugins: [lgZoom, lgThumbnail],
-    //licenseKey: 'your_license_key',
-    speed: 1000,
-    download: false,
-    allowMediaOverlap: true,
-    mobileSettings: {
-      controls: false,
-      showCloseIcon: true
-    }
-    // ... other settings
-  });
-});
 
 
-/*Header anchor links
-*********************************************/
-$(document).ready(function(){
-  $('header li a[href^="#"], .swiper-slide .btn').on('click',function (e) {
-      e.preventDefault();
-      var target = this.hash,
-      $target = $(target);
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top - 70
-      }, 500, 'swing');
-  });
-}); 
 
 /*Progress Bar scroll
 **********************************************/
